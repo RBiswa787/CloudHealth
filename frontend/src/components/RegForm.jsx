9ac18c204a4ef5e07ed5f804739416b91fd44ca6
Biswadeep Ray <biswa7102raydeep@gmail.com>
2022-10-17 15:55:53 +0530

    auth_test service

--- frontend/src/components/RegForm.jsx
+++ frontend/src/components/RegForm.jsx
@@ -1,6 +1,7 @@
 import {React,useState} from 'react'
-import {Paper,makeStyles,TextField} from '@material-ui/core';
-import useWindowDimensions from '../Hooks/useWindowDimensions'
+import {Paper,makeStyles,TextField,Button} from '@material-ui/core';
+import useWindowDimensions from '../Hooks/useWindowDimensions';
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
     paper : {
@@ -26,15 +27,22 @@ function RegForm() {
  const classes = useStyles();
   const {height,width} = useWindowDimensions();
   const [username,setUsername] = useState("Type your name!");
+  const [password,setPassword] = useState("Enter Password!");
   const handleUsername = (e) => {
     setUsername(e);
+  }
+  const handlePassword = (e) => {
+    setPassword(e);
+  }
+  const handleSubmit = () => {
+    console.log("submit clicked");
+    const payload = {username:username,password:password};
+    axios.post('http://localhost:8686/api/user/',payload)
+    .then(response => {alert(response)});
   }
   return (
     <div style={{height:height,width:width,backgroundColor:"blue",display:"flex",justifyContent: "center",alignItems: "center"}}>
         <Paper className={classes.paper}>
-            <Paper className = {classes.subpaper}>
-                {username}
-            </Paper>
             <Paper className = {classes.subpaper}>
             <TextField 
             id="outlined-basic" 
@@ -42,6 +50,14 @@ function RegForm() {
             value = {username} 
             onChange = {(e)=>{handleUsername(e.target.value)}}/>
             </Paper>
+            <Paper className = {classes.subpaper}>
+            <TextField 
+            id="outlined-basic" 
+            label="Password" 
+            value = {password} 
+            onChange = {(e)=>{handlePassword(e.target.value)}}/>
+            </Paper>
+            <Button onClick={handleSubmit()}>Submit</Button>
         </Paper>
     </div>
   )
