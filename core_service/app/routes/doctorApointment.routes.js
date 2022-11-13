a08c65353ea9090c4f407251771f1550439ace2d
Biswadeep Ray <biswa7102raydeep@gmail.com>
2022-11-13 20:41:48 +0530

    resolve 2

--- core_service/app/routes/doctorApointment.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = app => {
-    const doctorAppointment = require("../controllers/doctorAppointment.controller");
-
-    let router = require("express").Router();
-
-    app.use("/api/doctorAppointment", router);
-
-    router.post("/createUpdate", doctorAppointment.createUpdate);
-};
